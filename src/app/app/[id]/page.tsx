c9410f1f6eb5c81a48243169a6964c30f95cea06
Moritz Graschopf <m.graschopf@gmail.com>
2025-07-22 19:26:00 +0200

    implemented participant form with validation using shadcn/ui and react-hook-form, improved layout structure for viewing and joining sessions

--- src/app/app/[id]/page.tsx
+++ src/app/app/[id]/page.tsx
@@ -1,19 +1,43 @@
 "use client"
 
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {useState} from "react";
+import {useForm} from "react-hook-form"
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Input} from "@/components/ui/input";
+
+const participantSchema = z.object({
+    password: z.string().min(8, {
+        error: "Passwort muss mindestens 8 Zeichen lang sein"
+    })
+})
 
 export default function Page() {
     const [joined, setJoined] = useState(false)
 
+    const form = useForm<z.infer<typeof participantSchema>>({
+        resolver: zodResolver(participantSchema),
+        defaultValues: {
+            password: ""
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof participantSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return joined ? (
         <div>joined</div>
     ) : (
-        <div className="h-full grid place-items-center">
-            <Tabs>
-                <TabsList defaultValue="viewer">
+        <div className="flex justify-center items-center h-screen w-screen fixed top-0 left-0">
+            <Tabs defaultValue="viewer">
+                <TabsList>
                     <TabsTrigger value="viewer">Zuschauer</TabsTrigger>
                     <TabsTrigger value="participant">Teilnehmer</TabsTrigger>
                 </TabsList>
@@ -21,13 +45,49 @@ export default function Page() {
                     <Card>
                         <CardHeader>
                             <CardTitle>Zuschauer</CardTitle>
-                            <CardDescription>Als Zuschauer Beitreten</CardDescription>
+                            <CardDescription>Als Zuschauer beitreten</CardDescription>
                         </CardHeader>
-                        <CardContent>
-                            <Button className="w-full" asChild>
+                        <CardFooter className="flex-col">
+                            <Button className="w-full">
                                 Beitreten
                             </Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="participant">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Teilnehmer</CardTitle>
+                            <CardDescription>Als Teilnehmer mit Kamera und Mikrofon beitreten</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            {/*TODO: mir reichts das hier geht nicht mach ich nächstes mal*/}
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Passwort</FormLabel>
+                                                <FormControl>
+                                                    <Input type="password" {...field} />
+                                                </FormControl>
+                                                <FormDescription>
+                                                    Passwort, welches vom Host festgelegt worden ist.
+                                                </FormDescription>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </form>
+                            </Form>
                         </CardContent>
+                        <CardFooter className="flex-col">
+                            <Button className="w-full">
+                                Beitreten
+                            </Button>
+                        </CardFooter>
                     </Card>
                 </TabsContent>
             </Tabs>
